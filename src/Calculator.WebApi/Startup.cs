008d9dc7d0639a495af930eee9f960124426d0fb
Artuom <kazlouski.artuom@gmail.com>
2017-11-10 18:02:29 +0300

    core components

--- src/Calculator.WebApi/Startup.cs
+++ src/Calculator.WebApi/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Autofac;
 using Microsoft.Owin;
 using Owin;
 
@@ -12,6 +13,30 @@ namespace Calculator.WebApi
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+
+            ContainerBuilder builder = new ContainerBuilder();
+
+            builder.RegisterModule<WebApiModule>()
+                .RegisterModule<DbConnectionModule>();
+
+            RegisterDependency(builder);
+            ILifetimeScope container = builder.Build();
+
+            ResolveComponents(container);
+
+            AddsAditionalComponentToOwinPipelene(app);
+
+            app.UseIdentityServerBearerTokenAuthentication(CreateAuthenticationOptions());
+
+            ResourceAuthorizationRegistration(app);
+
+            HttpConfiguration httpConfiguration = container.Resolve<HttpConfiguration>();
+            ConFigureDocumentations(httpConfiguration);
+
+            app
+                .UseAutofacMiddleware(container)
+                .UseAutofacWebApi(httpConfiguration)
+                .UseWebApi(httpConfiguration);
         }
     }
 }
---
+++ src/Calculator.WebApi/WebApiModule.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ExceptionHandling;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Serilog;
+
+namespace Calculator.WebApi
+{
+    public class WebApiModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+
+            builder.RegisterApiControllers(ThisAssembly);
+
+            HttpConfiguration httpConfiguration = new HttpConfiguration();
+            httpConfiguration.MapHttpAttributeRoutes();
+            httpConfiguration.Services.Replace(typeof(IExceptionHandler), new GlobalExceptionHandler());
+
+            builder.RegisterInstance(httpConfiguration).AsSelf();
+        }
+    }
+
+    public class GlobalExceptionHandler : ExceptionHandler
+    {
+
+        static ILogger _logger;
+
+
+        private static ILogger Logger => (_logger ?? Log.Logger).ForContext<GlobalExceptionHandler>();
+
+        private readonly Dictionary<Type, HttpStatusCode> _errorsDictionary = new Dictionary<Type, HttpStatusCode>
+        {
+           
+        };
+
+
+
+        public override void Handle(ExceptionHandlerContext context)
+        {
+
+            base.Handle(context);
+            var errorType = context.Exception.GetType();
+            var error = _errorsDictionary.Where(s => s.Key == errorType)
+                .Select(e => (KeyValuePair<Type, HttpStatusCode>?)e)
+                .FirstOrDefault();
+            
+            var errorMessage =  context.Exception.Message;
+
+            var stausCode = error?.Value ?? HttpStatusCode.InternalServerError;
+            var resp = new HttpResponseMessage(stausCode)
+            {
+                Content = new StringContent(errorMessage),
+                ReasonPhrase = error == null ? context.Exception.GetType().ToString() : error.Value.Key.ToString(),
+
+            };
+            context.Result = new ErrorMessageResult(context.Request, resp);
+
+            if ((int)stausCode >= 500)
+                Logger.Error(context.Exception, context.Exception.Message);
+            else
+                Logger.Information(context.Exception, context.Exception.Message);
+
+        }
+
+        public class ErrorMessageResult : IHttpActionResult
+        {
+            private HttpRequestMessage _request;
+            private HttpResponseMessage _httpResponseMessage;
+
+
+            public ErrorMessageResult(HttpRequestMessage request, HttpResponseMessage httpResponseMessage)
+            {
+                _request = request;
+                _httpResponseMessage = httpResponseMessage;
+            }
+
+            public Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+            {
+                return Task.FromResult(_httpResponseMessage);
+            }
+        }
+    }
+}
